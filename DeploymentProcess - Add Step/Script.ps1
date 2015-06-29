6447d85bdd8ae94b4b2c2490c7bda02e6400109b
Dalmiro <dalmiro.granias@gmail.com>
2015-06-29 00:51:29 -0300

    Adding

---
+++ DeploymentProcess - Add Step/Script.ps1
@@ -0,0 +1,40 @@
+﻿$OctopusURL = $env:OctopusURL #Octopus URL
+$OctopusAPIKey = $env:OctopusAPIKey #Octopus API Key
+$header = @{ "X-Octopus-ApiKey" = $env:OctopusAPIKey }
+
+#Project to add the step
+$ProjectName = "Powershell3"
+
+#File with the Json of the new step/s
+$StepFile = "$PSScriptRoot\step.json"
+
+function Get-OctopusResource([string]$uri) {    
+    return Invoke-RestMethod -Method Get -Uri "$OctopusURL/$uri" -Headers $header
+}
+function Put-OctopusResource([string]$uri, [object]$resource) {
+    Invoke-RestMethod -Method Put -Uri "$OctopusURL/$uri" -Body $($resource | ConvertTo-Json -Depth 10) -Headers $header    
+}
+
+#Getting Project
+$Project = Get-OctopusResource /api/projects/$ProjectName
+
+#Getting Project's deployment process
+$DeploymentProcess = Get-OctopusResource $Project.Links.DeploymentProcess
+
+#Converting Json step into a PS Object. 
+#If you'd rather get the step from someplace else than a file, just make sure to set the Json as the value of $Step
+$step = Get-Content $StepFile | ConvertFrom-Json
+
+#Changing colletion to ArrayList to be able to add steps
+[System.Collections.ArrayList]$Steps = $DeploymentProcess.Steps
+
+#Adding step to ArrayList collection. The step will be added to the bottom.
+$Steps.Add($Step)
+
+#If you want to re-order the steps, you should do it here on $steps
+
+#Adding new steps array to Deployment process
+$DeploymentProcess.Steps = $Steps
+
+#Saving Deployment process to database
+Put-OctopusResource -uri $DeploymentProcess.Links.Self -resource $DeploymentProcess
