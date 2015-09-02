72d1a3700ef654b2417683271483ff54871b320b
Dalmiro <dalmiro.granias@gmail.com>
2015-09-02 10:54:57 -0300

    adding script

---
+++ Nuspec_Modify.ps1
@@ -0,0 +1,14 @@
+﻿$Nuspec = "C:\Test\MyApp.nuspec"
+[xml]$xml = Get-Content $Nuspec
+
+#Changing Release notes
+$xml.package.metadata.releaseNotes = "Some new release notes"
+
+#Changing version
+$xml.package.metadata.version = "1.0.0"
+
+#Changing description
+$xml.package.metadata.description = "New description"
+
+#saving nuspec changes
+$xml.Save($Nuspec)
