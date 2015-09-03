b7131b4c52152fcee03be9a9faf85ae2249e5a59
Dalmiro <dalmiro.granias@gmail.com>
2015-09-03 14:46:39 -0300

    Adding script

---
+++ Machines_Remove_Octoposh.ps1
@@ -0,0 +1,7 @@
+﻿#all offline machines of the instance
+$OfflineMachines = Get-OctopusMachine | ?{$_.status -eq "offline"}
+
+#Offline machines by environment. See the rest of the filters of this cmdlet on https://github.com/Dalmirog/OctoPosh/wiki/Get-OctopusMachine
+#$OfflineMachines = Get-OctopusMachine -EnvironmentName "Development" | ?{$_.status -eq "offline"}
+
+Remove-OctopusResource -Resource $OfflineMachines.resource -Wait #add -force to avoid prompt.
