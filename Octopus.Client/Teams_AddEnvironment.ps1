d8aedb2f6b8a7332413e502e6673cddb5c7f36b6
Dalmiro <dalmiro.granias@gmail.com>
2015-09-17 13:14:00 -0300

    adding script

---
+++ Octopus.Client/Teams_AddEnvironment.ps1
@@ -0,0 +1,17 @@
+﻿Add-Type -Path "Newtonsoft.Json.dll"
+Add-Type -Path "Octopus.Client.dll"
+
+$OctopusURL = ""#Your Octopus URL
+$OctopusAPIKey = "" #Your Octopus API Key
+
+$TeamName = ""#Name of the team to which you want to add the environments
+$EnvironmentName = ""#Environment you want to add
+
+$endpoint = new-object Octopus.Client.OctopusServerEndpoint $OctopusURL,$OctopusAPIKey    
+$repository = new-object Octopus.Client.OctopusRepository $endpoint                     
+
+$team = $repository.Teams.FindByName($TeamName)
+$team.EnvironmentIds.Add($EnvironmentName)
+$team.EnvironmentIds
+
+$repository.Teams.Modify($team)
