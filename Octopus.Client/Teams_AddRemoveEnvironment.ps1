78f2bae3c9a44989ce7b6504a02c5bfe56bbf5dd
Dalmiro <dalmiro.granias@gmail.com>
2015-09-17 13:28:22 -0300

    adding remove functionality

--- Octopus.Client/Teams_AddRemoveEnvironment.ps1
+++ Octopus.Client/Teams_AddRemoveEnvironment.ps1
@@ -8,13 +8,16 @@ Add-Type -Path "Octopus.Client.dll"
 $OctopusURL = ""#Your Octopus URL
 $OctopusAPIKey = "" #Your Octopus API Key
 
-$TeamName = ""#Name of the team to which you want to add the environments
-$EnvironmentName = ""#Environment you want to add
+$TeamName = ""#Name of the team to which you want to add/remove the environments
+$EnvironmentName = ""#Environment you want to add/remove
 
 $endpoint = new-object Octopus.Client.OctopusServerEndpoint $OctopusURL,$OctopusAPIKey    
 $repository = new-object Octopus.Client.OctopusRepository $endpoint                     
 
 $team = $repository.Teams.FindByName($TeamName)
-$team.EnvironmentIds.Add($EnvironmentName)
+$Environment = $repository.Environments.FindByName($EnvironmentName)
 
-$repository.Teams.Modify($team)
+$team.EnvironmentIds.Add($Environment.id) #<-- To add environment
+#$team.EnvironmentIds.Remove($Environment.id) #<-- To remove environment
+
+$repository.Teams.Modify($team) #<-- Save changes on the database
