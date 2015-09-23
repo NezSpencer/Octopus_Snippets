8518a168c3e57f84f259a481b66fc0e490a9cc2d
Dalmiro <dalmiro.granias@gmail.com>
2015-09-23 13:47:08 -0300

    Adding script

---
+++ Octopus.Client/Machines_DeleteByRole.ps1
@@ -0,0 +1,17 @@
+﻿Add-Type -Path "Newtonsoft.Json.dll" 
+Add-Type -Path "Octopus.Client.dll" 
+
+$OctopusURL = ""#Your Octopus URL
+$OctopusAPIKey = "" #Your Octopus API Key
+$Role = "" #All the machines with this role will be removed from all the environments
+
+$endpoint = new-object Octopus.Client.OctopusServerEndpoint $OctopusURL,$OctopusAPIKey    
+$repository = new-object Octopus.Client.OctopusRepository $endpoint     
+
+$AllMachines = $repository.repository.Machines.FindAll()
+
+$MachinesWithRole = $AllMachines | ?{$role -in $_.roles}
+
+foreach ($machine in $MachinesWithRole){
+    $repository.repository.Machines.Delete($machine)
+}
